b86f686c58435be104f40bea91f37104575316f7
Sanjay Singh <118108567+urssanjaysingh@users.noreply.github.com>
2023-07-31 16:35:12 +0530

    Add files via upload

---
+++ 005. Persistent User Login/src/components/PersistLogin.js
@@ -0,0 +1,50 @@
+import { Outlet } from "react-router";
+import { useState, useEffect, useRef } from "react";
+import useRefreshToken from '../hooks/useRefreshToken';
+import useAuth from "../hooks/useAuth";
+
+const PersistLogin = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const refresh = useRefreshToken();
+    const { auth, persist } = useAuth();
+    const effectRun = useRef(false);
+
+    useEffect(() => {
+        let isMounted = true;
+
+        const verifyRefreshToken = async () => {
+            try {
+                await refresh();
+            } catch (err) {
+                console.error(err);
+            } finally {
+                isMounted && setIsLoading(false);
+            }
+        }
+
+        !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
+
+        return () => isMounted = false;
+    }, [auth?.accessToken, refresh])
+
+    useEffect(() => {
+        if (effectRun.current) {
+            console.log(`isLoading: ${isLoading}`)
+            console.log(`aT: ${JSON.stringify(auth?.accessToken)}`)
+        }
+        effectRun.current = true;
+    }, [auth?.accessToken, isLoading])
+
+    return (
+        <>
+            {!persist
+                ? <Outlet />
+                : isLoading
+                    ? <p>isLoading...</p>
+                    : <Outlet />
+            }
+        </>
+    )
+}
+
+export default PersistLogin
